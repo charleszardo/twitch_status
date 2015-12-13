6c0617c387d712fdac2e6f34a39036a330d72fde
Dave Charnuska <davecharnuska@gmail.com>
2015-12-13 18:58:39 -0500

    begin trying get other user data

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var usernames = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"];
 var baseUrl = "https://api.twitch.tv/kraken/streams/";
-var cb = "?callback=?";
+var cb = "?client_id=566e055ff2217f3607bea86b&callback=?"
 
 $(document).ready(function(){
 	usernames.forEach(function(name) {
@@ -10,6 +10,15 @@ $(document).ready(function(){
 				status = "online";
 			}
 		  $("#channels").prepend('<div class="user ' + status + '">' + name + '</div>');
+		});
+		
+		$.getJSON(baseUrl + 'users/' + name + cb, function(data) {
+			console.log(data);
+			// var status = "offline";
+			// if (data.stream) {
+			// 	status = "online";
+			// }
+			// 		  $("#channels").prepend('<div class="user ' + status + '">' + name + '</div>');
 		});
 	});
 	
