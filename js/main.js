fb588aff1962ccb396beb6b09847a76f27e4423e
Dave Charnuska <davecharnuska@gmail.com>
2015-12-14 14:26:23 -0500

    fix global vs. local issue with userObj variable

--- js/main.js
+++ js/main.js
@@ -4,29 +4,37 @@ var cb = "?client_id=566e055ff2217f3607bea86b&callback=?"
 
 $(document).ready(function(){
 	usernames.forEach(function(username) {
-		userObj = {};
+		var userObj = {};
 		userObj.status = "offline";
+		userObj.summary = null;
 		$.getJSON(baseUrl + "streams/" + username + cb, function(data) {
+			return data;
+		}).done(function(data){
 			if (data.stream) {
 				userObj.status = "online";
+				userObj.summary = data.stream.channel.status;
 			}
+			
+			$.getJSON(baseUrl + 'users/' + username + cb, function(data) {
+				return data;
+			}).done(function(data){
+				if (!data.logo) {
+					data.logo = "http://placehold.it/50x50";
+				}
+				userObj.photo = data.logo;
+				userObj.display = data.display_name;
+			
+				$("#channels").prepend('<div class="user ' + userObj.status + '">' + 
+				'<a href="http://www.twitch.tv/' + username +'">' + 
+					'<div class="picture"><img class="logo" src="' + userObj.photo + '"' + '</div>' +
+					'<div class="username">' + userObj.display + '</div>' +
+					'<div class="current-stream">' + '</div>' +
+				'</a></div>'
+				);
+			});
 		});
 		
-		$.getJSON(baseUrl + 'users/' + username + cb, function(data) {
-			console.log(data);
-			if (!data.logo) {
-				data.logo = "http://placehold.it/50x50";
-			}
-			userObj.photo = data.logo;
-		}).done(function(){
-			$("#channels").prepend('<div class="user ' + userObj.status + '">' + 
-				'<div class="picture"><img class="logo" src="' + userObj.photo + '"' + '</div>' +
-				'<div class="username">' + 
-				'<a href="http://www.twitch.tv/' + username +'">' + username + '</a></div>' +
-				'<div class="current-stream">' + '</div>' +
-			'</div>'
-			);
-		});
+
 		
 
 	});
@@ -36,12 +44,12 @@ $(document).ready(function(){
 	});
 	
 	$("#online").click(function(){
-		$(".online").show();
-		$(".offline").hide();
+		$(".online").children().show();
+		$(".offline").children().hide();
 	});
 	
 	$("#offline").click(function(){
-		$(".offline").show();
-		$(".online").hide();
+		$(".offline").children().show();
+		$(".online").children().hide();
 	});
 })
