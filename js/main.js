f1cb48ffe40c96bf9217deb4de4a47db79b6f49e
Dave Charnuska <davecharnuska@gmail.com>
2015-12-15 12:31:16 -0500

    add checkStatus and toggleStatus

--- js/main.js
+++ js/main.js
@@ -1,9 +1,41 @@
-var usernames = ["brunofin", "medrybw", "freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"];
+var usernames = {
+	"brunofin": false,
+	"medrybw": false,
+	"freecodecamp": false,
+	"storbeck": false,
+	"terakilobyte": false,
+	"habathcx": false,
+	"RobotCaleb": false,
+	"thomasballinger": false,
+	"noobs2ninjas": false,
+	"beohoff": false
+};
+
 var baseUrl = "https://api.twitch.tv/kraken/";
 var cb = "?client_id=566e055ff2217f3607bea86b&callback=?"
 
 $(document).ready(function(){
-	usernames.forEach(function(username) {
+	function checkStatus() {
+		Object.keys(usernames).forEach(function(username) {
+			$.getJSON(baseUrl + "streams/" + username + cb, function(data) {
+				return data;
+			}).done(function(data){
+				if (data.stream && !usernames[username]) {
+					// user has signed on
+					toggleStatus(username, true);
+				} else if (!data.stream && usernames[username]) {
+					// user has signed off
+					toggleStatus(username, false);
+				}
+			});
+		})
+	}
+	
+	function toggleStatus(username, on){
+		usernames[username] = on;
+	}
+	
+	Object.keys(usernames).forEach(function(username) {
 		var userObj = {};
 		userObj.status = "offline";
 		userObj.summary = null;
@@ -12,6 +44,7 @@ $(document).ready(function(){
 			return data;
 		}).done(function(data){
 			if (data.stream) {
+				usernames[username] = true;
 				userObj.status = "online";
 				userObj.summary = data.stream.channel.status;
 				userObj.icon = '<div class="icon-on">&#10003</div>';
@@ -26,7 +59,7 @@ $(document).ready(function(){
 				userObj.photo = data.logo;
 				userObj.display = data.display_name;
 			
-				$("#channels").prepend('<div class="user ' + userObj.status + '">' + 
+				$("#channels").prepend('<div id="' + username + '" class="user ' + userObj.status + '">' + 
 				'<a href="http://www.twitch.tv/' + username +'">' + 
 					'<div class="picture"><img class="logo" src="' + userObj.photo + '"' + '</div>' +
 					'<div class="username">' + userObj.display + '</div>' +
@@ -36,6 +69,9 @@ $(document).ready(function(){
 				);
 			});
 		});
+		
+		setInterval(checkStatus, 1000);
+		setInterval(updateStatus, 1000);
 	});
 	
 	$("#all").click(function(){
